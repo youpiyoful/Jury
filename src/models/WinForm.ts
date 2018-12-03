6a1aa00dea39292421774561db0e65c58569c989
youpiyoful <youpiyoful@Host-002.lan>
2018-12-03 02:11:02 +0100

    [wip] populated winForm models and clear winsService

--- src/models/WinForm.ts
+++ src/models/WinForm.ts
@@ -1,3 +1,13 @@
 export class WinForm {
-  constructor(public name: string) {}
+  constructor(public idForm: number,
+              public idTable: number,
+              public idWin: number,
+              public blindTest: number,
+              public winAndBottleTest: number,
+              public bottleShape: string,
+              public bottleColor: string,
+              public bottleCap: string,
+              public labelColor: string,
+              public labelShape: string,
+              public labelMessage: string) {}
 }
--- src/services/wins.service.ts
+++ src/services/wins.service.ts
@@ -2,31 +2,4 @@ import { WinForm } from '../models/WinForm';
 
 export class WinsService {
   winFormList: WinForm[] = []; //surveyList think for name of array
-//   obj = {
-//     "0": 1002104, // identifiant : numéro table + numéro vin
-//     "1": 1002, // numéro de table
-//     "2": 104, // numéro du vin en cours
-//     "3": 1, // index du nième vin
-//     "4": 2.05,
-//     "5": "7.22",
-//     "6": "positif",
-//     "7": "neutre",
-//     "8": "negatif",
-//     "9": "neutre",
-//     "10": "negatif",
-//     "11": "positif",
-//     "12": "negatif",
-//     "13": "positif",
-//     "14": "neutre",
-//     "15": "Mon commentaire ",
-//     "16": 0, // A boire en cours de repas
-//     "17": 1, // Parfait pour l'apéritif
-//     "18": 0, // Parfait pour le dessert
-//     "19": 1, // Pour les grandes étapes de la vie
-//     "20": 0, // A l'occasion d'un vin d'honneur
-//     "21": "15-20"
-//   };
-//
-//
-//   arr = Object.keys(this.obj).map(function(k) { return this.obj[k] });
-// }
+}
