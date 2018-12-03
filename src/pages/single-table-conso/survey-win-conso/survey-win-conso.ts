889c6cceea76c9f77a3758ab9e5799921ceecda8
youpiyoful <youpiyoful@Host-002.lan>
2018-12-03 22:50:02 +0100

    [fix] i fix problem with iteratorWin

--- src/pages/single-table-conso/survey-win-conso/survey-win-conso.ts
+++ src/pages/single-table-conso/survey-win-conso/survey-win-conso.ts
@@ -17,9 +17,10 @@ export class SurveyWinConsoPage implements OnInit {
               public navParams: NavParams) {}
 
   ngOnInit() {
-    this.idWin =+ this.navParams.get('idSurvey').toString().slice(0,3); // i use toString for explode the array and select what i want
-    this.idTable =+ this.navParams.get('idSurvey').toString().slice(3,6); // slice for select what i want in the new array
-    this.iteratorWin =+ this.navParams.get('idSurvey').toString().slice(6);
+    this.idTable =+ this.navParams.get('idSurvey').toString().slice(0,3); // i use toString for explode the array and select what i want
+    this.idWin =+ this.navParams.get('idSurvey').toString().slice(3,6); // slice for select what i want in the new array
+    console.log(this.idWin);
+    this.iteratorWin =+ this.navParams.get('idSurvey').toString().slice(6) + 1;
     this.idForm =+ this.navParams.get('idSurvey');
   }
 }
